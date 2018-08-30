e18769ddafcbe3e044d51cfb888119e9b74ec438
Hacktiv8 <hacktiv8@H8JKT003.localdomain>
2018-08-30 14:07:40 +0700

    nambah isi tabel dan form addexpense.ejs

--- controllers/expense.js
+++ controllers/expense.js
@@ -2,8 +2,19 @@ const { User, Expense, Category } = require('../models/')
 
 class Controller {
     
-    static list(req,res) {
-        res.send('echo expense')
+    static list(req,res, errNotif) {
+        // res.send('echo expense')
+        Expense.findAll({
+            order : [
+                ['createdAt','DESC']
+            ]
+        })
+        .then(exp => {
+            res.render('addExpense', {exp, errNotif})
+        })
+        .catch(err => {
+            res.send(err)
+        })
     }
     
 }
